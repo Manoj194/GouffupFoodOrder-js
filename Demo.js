c165a5b17eb4f174b2ac4bb3398c528ceb397483
Manoj194 <manuvirat772@gmail.com>
2022-09-29 11:45:21 +0530

    commit done

---
+++ Demo.js
@@ -0,0 +1,15 @@
+var a=10
+console.log(typeof(a))
+var b='a'
+console.log(typeof(b))
+var c="manoj"
+console.log(typeof(c))
+var d=12.2558
+console.log(typeof(d))
+var e=null
+console.log(typeof(e))
+var f
+console.log(typeof(f))
+var g=true
+console.log(typeof(g));
+
